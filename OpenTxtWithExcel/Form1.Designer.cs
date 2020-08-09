cfa384bfcd37b5b021718e3b82545d544f76d891
AiSY Yang <kai978854518@hotmail.com>
2020-08-09 18:35:48 +0800

    添加项目文件。

---
+++ OpenTxtWithExcel/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace OpenTxtWithExcel
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.groupBox3 = new System.Windows.Forms.GroupBox();
+			this.checkBox4 = new System.Windows.Forms.CheckBox();
+			this.groupBox4 = new System.Windows.Forms.GroupBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.checkBox6 = new System.Windows.Forms.CheckBox();
+			this.checkBox5 = new System.Windows.Forms.CheckBox();
+			this.checkBox3 = new System.Windows.Forms.CheckBox();
+			this.checkBox2 = new System.Windows.Forms.CheckBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.groupBox3.SuspendLayout();
+			this.groupBox4.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(6, 32);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(0, 12);
+			this.label1.TabIndex = 1;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Location = new System.Drawing.Point(12, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(366, 306);
+			this.groupBox1.TabIndex = 2;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "拖拽进此区域";
+			this.groupBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.checkBox2);
+			this.groupBox2.Controls.Add(this.checkBox1);
+			this.groupBox2.Location = new System.Drawing.Point(386, 148);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(200, 77);
+			this.groupBox2.TabIndex = 3;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "单文件行为";
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Enabled = false;
+			this.checkBox1.Location = new System.Drawing.Point(7, 21);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(84, 16);
+			this.checkBox1.TabIndex = 0;
+			this.checkBox1.Text = "删除源文件";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// groupBox3
+			// 
+			this.groupBox3.Controls.Add(this.checkBox3);
+			this.groupBox3.Controls.Add(this.checkBox4);
+			this.groupBox3.Location = new System.Drawing.Point(386, 241);
+			this.groupBox3.Name = "groupBox3";
+			this.groupBox3.Size = new System.Drawing.Size(200, 77);
+			this.groupBox3.TabIndex = 4;
+			this.groupBox3.TabStop = false;
+			this.groupBox3.Text = "多文件行为";
+			// 
+			// checkBox4
+			// 
+			this.checkBox4.AutoSize = true;
+			this.checkBox4.Enabled = false;
+			this.checkBox4.Location = new System.Drawing.Point(7, 21);
+			this.checkBox4.Name = "checkBox4";
+			this.checkBox4.Size = new System.Drawing.Size(84, 16);
+			this.checkBox4.TabIndex = 0;
+			this.checkBox4.Text = "删除源文件";
+			this.checkBox4.UseVisualStyleBackColor = true;
+			// 
+			// groupBox4
+			// 
+			this.groupBox4.Controls.Add(this.checkBox6);
+			this.groupBox4.Controls.Add(this.checkBox5);
+			this.groupBox4.Location = new System.Drawing.Point(386, 89);
+			this.groupBox4.Name = "groupBox4";
+			this.groupBox4.Size = new System.Drawing.Size(200, 53);
+			this.groupBox4.TabIndex = 5;
+			this.groupBox4.TabStop = false;
+			this.groupBox4.Text = "分隔符";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(386, 26);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(233, 12);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "请拖拽文件至左边窗口,按照UTF-8编码读取";
+			// 
+			// checkBox6
+			// 
+			this.checkBox6.AutoSize = true;
+			this.checkBox6.Checked = global::OpenTxtWithExcel.Properties.Settings.Default.SplitCharTab;
+			this.checkBox6.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.checkBox6.DataBindings.Add(new System.Windows.Forms.Binding("Checked", global::OpenTxtWithExcel.Properties.Settings.Default, "SplitCharTab", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+			this.checkBox6.Location = new System.Drawing.Point(91, 20);
+			this.checkBox6.Name = "checkBox6";
+			this.checkBox6.Size = new System.Drawing.Size(42, 16);
+			this.checkBox6.TabIndex = 1;
+			this.checkBox6.Text = "Tab";
+			this.checkBox6.UseVisualStyleBackColor = true;
+			// 
+			// checkBox5
+			// 
+			this.checkBox5.AutoSize = true;
+			this.checkBox5.Checked = global::OpenTxtWithExcel.Properties.Settings.Default.SplitCharComma;
+			this.checkBox5.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.checkBox5.DataBindings.Add(new System.Windows.Forms.Binding("Checked", global::OpenTxtWithExcel.Properties.Settings.Default, "SplitCharComma", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+			this.checkBox5.Location = new System.Drawing.Point(7, 21);
+			this.checkBox5.Name = "checkBox5";
+			this.checkBox5.Size = new System.Drawing.Size(48, 16);
+			this.checkBox5.TabIndex = 0;
+			this.checkBox5.Text = "逗号";
+			this.checkBox5.UseVisualStyleBackColor = true;
+			// 
+			// checkBox3
+			// 
+			this.checkBox3.AutoSize = true;
+			this.checkBox3.Checked = global::OpenTxtWithExcel.Properties.Settings.Default.MultiFileRun;
+			this.checkBox3.DataBindings.Add(new System.Windows.Forms.Binding("Checked", global::OpenTxtWithExcel.Properties.Settings.Default, "MultiFileRun", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+			this.checkBox3.Location = new System.Drawing.Point(7, 44);
+			this.checkBox3.Name = "checkBox3";
+			this.checkBox3.Size = new System.Drawing.Size(114, 16);
+			this.checkBox3.TabIndex = 1;
+			this.checkBox3.Text = "自动用Excel打开";
+			this.checkBox3.UseVisualStyleBackColor = true;
+			// 
+			// checkBox2
+			// 
+			this.checkBox2.AutoSize = true;
+			this.checkBox2.Checked = global::OpenTxtWithExcel.Properties.Settings.Default.SingleFileRun;
+			this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.checkBox2.DataBindings.Add(new System.Windows.Forms.Binding("Checked", global::OpenTxtWithExcel.Properties.Settings.Default, "SingleFileRun", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+			this.checkBox2.Location = new System.Drawing.Point(7, 44);
+			this.checkBox2.Name = "checkBox2";
+			this.checkBox2.Size = new System.Drawing.Size(114, 16);
+			this.checkBox2.TabIndex = 1;
+			this.checkBox2.Text = "自动用Excel打开";
+			this.checkBox2.UseVisualStyleBackColor = true;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "UTF-8",
+            "GB2312"});
+			this.comboBox1.Location = new System.Drawing.Point(465, 44);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(121, 20);
+			this.comboBox1.TabIndex = 7;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(386, 51);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(59, 12);
+			this.label3.TabIndex = 8;
+			this.label3.Text = "文件编码:";
+			// 
+			// Form1
+			// 
+			this.AllowDrop = true;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(641, 330);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.groupBox4);
+			this.Controls.Add(this.groupBox3);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+			this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			this.groupBox3.ResumeLayout(false);
+			this.groupBox3.PerformLayout();
+			this.groupBox4.ResumeLayout(false);
+			this.groupBox4.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.CheckBox checkBox2;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.GroupBox groupBox3;
+		private System.Windows.Forms.CheckBox checkBox3;
+		private System.Windows.Forms.CheckBox checkBox4;
+		private System.Windows.Forms.GroupBox groupBox4;
+		private System.Windows.Forms.CheckBox checkBox6;
+		private System.Windows.Forms.CheckBox checkBox5;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label3;
+	}
+}
+
---
+++ OpenTxtWithExcel/Form1.cs
@@ -0,0 +1,147 @@
+﻿using OfficeOpenXml;
+using OfficeOpenXml.Style;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OpenTxtWithExcel
+{
+	public partial class Form1 : Form
+	{
+		public FileDropHandler FileDroper;
+		public Form1()
+		{
+			InitializeComponent();
+			OfficeOpenXml.ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+			FileDroper = new FileDropHandler(this);
+			comboBox1.SelectedIndex = 0;
+		}
+
+		private void Form1_DragEnter(object sender, DragEventArgs e)
+		{
+			e.Effect = DragDropEffects.Copy;
+			var s = e.Data.GetData(typeof(string[])) as string[];
+			label1.Text = "";
+			List<string> filelist = new List<string>();
+			List<string> resultlist = new List<string>();
+			foreach (var item in s)
+			{
+				if (Directory.Exists(item))
+				{
+					string[] txtFile = Directory.GetFiles(item, "*.txt", SearchOption.AllDirectories);
+					foreach (var file in txtFile)
+					{
+						filelist.Add(file);
+					}
+					string[] csvFile = Directory.GetFiles(item, "*.csv", SearchOption.AllDirectories);
+					foreach (var file in csvFile)
+					{
+						filelist.Add(file);
+					}
+				}
+				if (File.Exists(item) && (Path.GetExtension(item).Equals(".txt", StringComparison.OrdinalIgnoreCase) || Path.GetExtension(item).Equals(".csv", StringComparison.OrdinalIgnoreCase)))
+				{
+					filelist.Add(item);
+				}
+			}
+			if (filelist.Count == 0)
+			{
+				label1.Text = "无有效文件";
+				return;
+			}
+			label1.Text = DateTime.Now.ToLongTimeString() + " 处理中" + "\n";
+			DateTime startTime = DateTime.Now;
+			foreach (var item in filelist)
+			{
+				label1.Text += DateTime.Now.ToLongTimeString() +" "+ Path.GetFileName(item) + "\n";
+				resultlist.Add(ProcessFile(item));
+				this.Refresh();
+			}
+
+			if (filelist.Count == 1)
+			{
+				if (checkBox2.Checked)
+				{
+					Process.Start(resultlist[0]);
+				}
+			}
+			else
+			{
+				if (checkBox3.Checked)
+				{
+					foreach (var item in resultlist)
+					{
+						Process.Start(item);
+					}
+				}
+			}
+			label1.Text += DateTime.Now.ToLongTimeString() + " 处理完毕" + "\n";
+			label1.Text += filelist.Count + "个文件共用时" + (DateTime.Now - startTime).ToString("T") + "\n";
+
+			return;
+		}
+
+		string ProcessFile(string file)
+		{
+			using (OfficeOpenXml.ExcelPackage excel = new OfficeOpenXml.ExcelPackage())
+			{
+				char[] splitchar = new char[2] { checkBox5.Checked ? ',' : '\n', checkBox6.Checked ? '\t' : '\n' };
+				OfficeOpenXml.ExcelWorksheet ws = excel.Workbook.Worksheets.Add(Path.GetFileNameWithoutExtension(file));
+				string[] s = File.ReadAllLines(file,Encoding.GetEncoding(comboBox1.SelectedItem.ToString()));
+				if (s.Count() == 0)
+				{
+					label1.Text = "读取失败,文件可能为空";
+				}
+				DataTable dataTable = new DataTable();
+				dataTable.BeginLoadData();
+				foreach (var item in s[0].Split(splitchar))
+				{
+					try
+					{
+					dataTable.Columns.Add(item, typeof(string));
+
+					}
+					catch (Exception)
+					{
+						label1.Text += "读取失败,表头重复"+"\n";
+						throw;
+					}
+				}
+				foreach (var item in s.Skip(1))
+				{
+					dataTable.Rows.Add(item.Split(splitchar));
+				}
+				dataTable.EndLoadData();
+				//识别数字的列 弃用 改为全部以字符串方式读取
+				//DataTable dataTable2 = new DataTable();
+				//foreach (DataColumn item in dataTable.Columns)
+				//{
+				//	if (dataTable.AsEnumerable().Select(x => x.Field<string>(item)).Where(x => int.TryParse(x, out _)).Count() == dataTable.Rows.Count)
+				//	{ dataTable2.Columns.Add(item.ColumnName, typeof(int)); }
+				//	else
+				//	{
+				//		dataTable2.Columns.Add(item.ColumnName, typeof(string));
+				//	}
+				//}
+				//dataTable2.Load(dataTable.CreateDataReader());
+				ws.Cells.LoadFromDataTable(dataTable, true);
+				ws.Cells.Style.Numberformat.Format = "@";
+				excel.SaveAs(new FileInfo(file.Remove(file.Length - 4) + ".xlsx"));
+				return file.Remove(file.Length - 4) + ".xlsx";
+			}
+		}
+
+		private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			Properties.Settings.Default.Save();
+		}
+	}
+}
